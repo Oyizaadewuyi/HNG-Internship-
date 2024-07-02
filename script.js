e639eca5ea61d10b50dfad2f8204694f694ec390
Oyizaadewuyi <oyizaadewuyi@gmail.com>
2024-07-02 17:43:05 +0100

    HNG TASK-1

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
+
     function updateTime() {
         const now = new Date();
+
         const utcTime=
         now.toUTCString();
         const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday' ];
+
         const currentDay = 
         days[now.getUTCDay()];
 
@@ -11,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         document.getElementById('currentDay').textContent =currentDay;
     }
+    
     updateTime();
     setInterval(updateTime,60000);//update every minutes
 
